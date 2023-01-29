6d8a73e301e139df3e71ddf9d3146c04c16e496f
konstantin.brikalov <dapmoed@gmail.com>
2023-01-29 21:14:25 +0500

    feat: drone config edit

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/dron-ci-test/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+			"message": "pong YHO-HO",
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
